c857eb0ab889a414c6f1a139f36b2de0a33f1247
Bonta-Vlad <bontavladgabriel@gmail.com>
2026-05-20 18:45:41 +0300

    menu run plus a few fixes

--- src/menu.cpp
+++ src/menu.cpp
@@ -2,9 +2,9 @@
 #include "ftxui/component/component.hpp"       
 #include "ftxui/component/component_base.hpp"  
 #include "ftxui/component/component_options.hpp"  
-#include "ftxui/dom/elements.hpp"  
 #include "ftxui/util/ref.hpp"
 #include "ftxui/component/app.hpp"
+#include <stdexcept>
 Menu* Menu::Instance= nullptr;
 int Menu::State= 1;
 
@@ -16,6 +16,7 @@ void Menu::connect(){
     std::string password;
     std::string hostname;
     std::string username;
+
     InputOption password_option;
     password_option.password = true;
     Component input_password = Input(&password, "password", password_option);
@@ -24,13 +25,22 @@ void Menu::connect(){
     Component input_user= Input(&username,"postgres");
     Component input_port= Input(&port, "5432");
 
-
     auto component = Container::Vertical({
     input_host,
     input_port,
     input_user,
     input_password
     });
+
     auto app = App::TerminalOutput();
     app.Loop(component);
+}
+
+void Menu::run(){
+    switch (Menu::State) {
+    case 0: return;break;
+    case 1: connect();break;
+    case 2: /*freeform()*/;break;
+    default: throw std::runtime_error("Invalid state!"); //TODO: Use custome exception
+    }
 }
